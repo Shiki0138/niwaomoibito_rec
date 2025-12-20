14d92c549a53effcf1c4fd4b8d85d775b6f432c2
Greenroom51 <greenroom51@gmail.com>
2025-12-20 19:24:08 +0900

    Initial commit: Niwaomoibito Recruitment LP

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Main JavaScript
+console.log('Niwaomoibito Recruitment LP Loaded');
+
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
